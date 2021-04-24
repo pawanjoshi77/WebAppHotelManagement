24623056d04c1d29736813ce785b3badd8c1e2a6
PawanXccelerata <pawan@xccelerata.com>
2021-04-24 13:38:46 -0400

    First Commit

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppHotelManagement.Models;
+using WebAppHotelManagement.ViewModel;
+
+namespace WebAppHotelManagement.Controllers
+{
+    public class RoomController : Controller
+    {
+        private HotelDBEntities objHotelDBEntities;
+        public RoomController()
+        {
+            objHotelDBEntities = new HotelDBEntities();
+
+        }
+        // GET: Room
+        public ActionResult Index()
+        {
+            RoomViewModel objRoomViewModel = new RoomViewModel();
+            objRoomViewModel.ListOfBookingStatus = (from obj in objHotelDBEntities.BookingStatus
+                                                    select new SelectListItem()
+                                                    {
+                                                        Text = obj.BookingStatus,
+                                                        Value = obj.BookingStatusId.ToString()
+                                                    }).ToList();
+            objRoomViewModel.ListOfRoomType = (from obj in objHotelDBEntities.RoomTypes
+                                                    select new SelectListItem()
+                                                    {
+                                                        Text = obj.RoomTypeName,
+                                                        Value = obj.RoomTypeId.ToString()
+                                                    }).ToList();
+            return View(objRoomViewModel);
+        }
+    }
+}
